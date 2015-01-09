1e77fae8465a494bbc0163a2ba00d01fdf5c5bc8
MaggiezzZ <ymjmaggie@126.com>
2015-01-09 10:37:35 +0800

    print day

--- Record0108/ViewController.swift
+++ Record0108/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println("helloworld")
+        println("day0109")
     }
 
     override func didReceiveMemoryWarning() {
